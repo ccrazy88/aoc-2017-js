159e94c5df6e030c8a1e2aa1d9457335df8d3604
Chrisna Aing <chrisna@chrisna.org>
2017-12-28 03:28:34 -0800

    Add Jest and solution for day 1.

---
+++ src/day-01.js
@@ -0,0 +1,25 @@
+const parseDigits = input =>
+  input
+    .split("")
+    .map(value => parseInt(value, 10))
+    .filter(value => !isNaN(value));
+
+const solveCaptcha = (input, findNextIndex) => {
+  const digits = parseDigits(input);
+  const { length } = digits;
+  return digits.reduce(
+    (accumulator, value, index) =>
+      value === digits[findNextIndex(index, length)]
+        ? value + accumulator
+        : accumulator,
+    0
+  );
+};
+
+const solveCaptchaFirstHalf = input =>
+  solveCaptcha(input, (index, length) => (index + 1) % length);
+
+const solveCaptchaSecondHalf = input =>
+  solveCaptcha(input, (index, length) => (index + length / 2) % length);
+
+module.exports = { solveCaptchaFirstHalf, solveCaptchaSecondHalf };
---
+++ wallaby.js
@@ -0,0 +1,13 @@
+module.exports = wallaby => {
+  return {
+    files: ["src/**/*.js"],
+    tests: ["test/**/*.js"],
+    env: {
+      type: "node"
+    },
+    compilers: {
+      "**/*.js": wallaby.compilers.babel()
+    },
+    testFramework: "jest"
+  };
+};
